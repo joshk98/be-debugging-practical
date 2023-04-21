fe5ec6be74622561513b131a702d3ac68096528a
Lisa Heffernan <lisaheffernan@outlook.com>
2023-04-21 10:43:20 +0100

    formatting

--- tests/debugging.test.js
+++ tests/debugging.test.js
@@ -58,23 +58,27 @@ describe("debugging", () => {
 
   xit("debug 7", async () => {
     const { status } = await request(app).get("/albums");
+
     expect(status).to.equal(200);
   });
 
   xit("debug 8", async () => {
     const { status } = await request(app).get("/login");
+
     expect(status).to.equal(200);
     expect(body).to.equal(`Welcome ${username}`);
   });
 
   xit("debug 9", async () => {
     const { status, body } = await request(app).get("/users");
+
     expect(status).toEqual(200);
     expect(body).toBe("array");
   });
 
   xit("debug 10", async () => {
     const { status, body } = await request(app).get("/books");
+
     expect(status).to.equal(201);
     expect(body).to.be.an("array");
     expect(body).to.have.length.greaterThan(3);
