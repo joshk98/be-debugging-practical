591199cb3fc7263b5e86305be3763921bb15bcc2
joshkkuhn@gmail.com <joshkkuhn@gmail.com>
2023-05-04 20:32:09 +0100

    First and final commit

--- src/app.js
+++ src/app.js
@@ -7,52 +7,51 @@ app.use(express.json());
 // debug tasks up to 7 have NO errors in the test, only in the code below here.
 
 // debug 1
-app.get("/helloworld", (_, res) => {
-  res.status(200).json("Hello World");
+app.get("/hello", (_, res) => {
+  res.status(200).json("Hello World!");
 });
 
 // debug 2
-app.get("/goodbye", (_, res) => {
+app.delete("/goodbye", (_, res) => {
   res.sendStatus(204);
 });
 
 // debug 3(a) and 3(b)
-app.post("/pokemon", (_, res) => {
-  res.status(200).json("Pikachu");
+app.post("/pokemon", (req, res) => {
+  res.status(201).json(req.body.pokemon);
 });
 
 // debug 4
 app.get("/pokemon", (_, res) => {
-  res.sendStatus(200).json("Catch them all!");
+  res.status(200).json("Catch them all!");
 });
 
 // debug 5
 app.delete("/pokemon", (_, res) => {
-  res.status(204);
+  res.sendStatus(204);
 });
 
 // debug 6
 app.patch("/greeting/:forename/:surname", (req, res) => {
-  const { forename, surname, age } = req.params;
+  const { forename, surname } = req.params;
+  const { age } = req.body;
   res.status(200).json(`Happy ${age}th birthday, ${forename} ${surname}!`);
 });
 
 // debug 7
 app.get("/pokemon/choose", (req, res) => {
-  const { name } = req.params;
-
-  res.status(200).json(`${name}, I choose you!`);
+  res.status(200).json(`${req.query.name}, I choose you!`);
 });
 
 // debug 8
-app.get("albums", (_, res) => {
+app.get("/albums", (_, res) => {
   res.status(200).json("Albums");
 });
 
 // The following tasks could have errors in the test as well in the code below.
 
 // debug 9
-app.get("/login", (req, res) => {
+app.get("/login/:username", (req, res) => {
   const { username } = req.params;
   res.status(200).json(`Welcome ${username}`);
 });
--- tests/debugging.test.js
+++ tests/debugging.test.js
@@ -3,20 +3,20 @@ const request = require("supertest");
 const app = require("../src/app");
 
 describe("debugging", () => {
-  xit("debug 1", async () => {
+  it("debug 1", async () => {
     const { status, body } = await request(app).get("/hello").send();
 
     expect(status).to.equal(200);
     expect(body).to.equal("Hello World!");
   });
 
-  xit("debug 2", async () => {
+  it("debug 2", async () => {
     const { status } = await request(app).delete("/goodbye").send();
 
     expect(status).to.equal(204);
   });
 
-  xit("debug 3(a)", async () => {
+  it("debug 3(a)", async () => {
     const { status, body } = await request(app)
       .post("/pokemon")
       .send({ pokemon: "Pikachu" });
@@ -25,7 +25,7 @@ describe("debugging", () => {
     expect(body).to.equal("Pikachu");
   });
 
-  xit("debug 3(b)", async () => {
+  it("debug 3(b)", async () => {
     const { status, body } = await request(app)
       .post("/pokemon")
       .send({ pokemon: "Squirtle" });
@@ -34,20 +34,20 @@ describe("debugging", () => {
     expect(body).to.equal("Squirtle");
   });
 
-  xit("debug 4", async () => {
+  it("debug 4", async () => {
     const { status, body } = await request(app).get("/pokemon").send();
 
     expect(status).to.equal(200);
     expect(body).to.equal("Catch them all!");
   });
 
-  xit("debug 5", async () => {
+  it("debug 5", async () => {
     const { status } = await request(app).delete("/pokemon").send();
 
     expect(status).to.equal(204);
   });
 
-  xit("debug 6", async () => {
+  it("debug 6", async () => {
     const { status, body } = await request(app)
       .patch("/greeting/Bilbo/Baggins")
       .send({ age: 111 });
@@ -56,7 +56,7 @@ describe("debugging", () => {
     expect(body).to.equal("Happy 111th birthday, Bilbo Baggins!");
   });
 
-  xit("debug 7", async () => {
+  it("debug 7", async () => {
     const { status, body } = await request(app)
       .get("/pokemon/choose?name=Pikachu")
       .send();
@@ -65,32 +65,34 @@ describe("debugging", () => {
     expect(body).to.equal("Pikachu, I choose you!");
   });
 
-  xit("debug 8", async () => {
+  it("debug 8", async () => {
     const { status } = await request(app).get("/albums");
 
     expect(status).to.equal(200);
   });
 
-  xit("debug 9", async () => {
-    const { status } = await request(app).get("/login");
+  it("debug 9", async () => {
+    const { status, body } = await request(app)
+      .get("/login/NoobMaster69")
+      .send();
 
     expect(status).to.equal(200);
-    expect(body).to.equal(`Welcome ${username}`);
+    expect(body).to.equal(`Welcome NoobMaster69`);
   });
 
-  xit("debug 10", async () => {
+  it("debug 10", async () => {
     const { status, body } = await request(app).get("/users");
 
-    expect(status).toEqual(200);
-    expect(body).toBe("array");
+    expect(status).to.equal(200);
+    expect(body).to.be.an("array");
   });
 
-  xit("debug 11", async () => {
+  it("debug 11", async () => {
     const { status, body } = await request(app).get("/books");
 
     expect(status).to.equal(201);
     expect(body).to.be.an("array");
-    expect(body).to.have.length.greaterThan(3);
+    expect(body).to.have.length.greaterThanOrEqual(3);
     expect(body[0]).to.have.property("id");
     expect(body[0]).to.have.property("title");
     expect(body[0]).to.have.property("author");
